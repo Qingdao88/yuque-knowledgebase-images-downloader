3341bd87f562158a4221aa229cc002cb5e8c753c
Qingdao88 <Qingdao88@proton.me>
2024-12-30 17:11:32 +0800

    配置打开已登录语雀的浏览器

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import puppeteer from 'puppeteer-core';
 
 const browser = await puppeteer.launch({
-    executablePath: 'A:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    executablePath: 'A:/Program Files/Google/Chrome/Application/chrome.exe',
+    userDataDir: 'A:/Users/Bette/AppData/Local/Google/Chrome/User Data',
+    headless: false,
 });
+
 const page = await browser.newPage();
 
 await page.goto('https://www.yuque.com');
 
 await page.setViewport({width: 1920, height: 1080});
-
-await page.screenshot({path: 'test.png'});
 
 await browser.close();
