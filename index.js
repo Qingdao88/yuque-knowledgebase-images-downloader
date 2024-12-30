ffa95b575b46cb412d78a7092d5a15e2db623efe
Qingdao88 <Qingdao88@proton.me>
2024-12-30 17:01:05 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,14 @@
+import puppeteer from 'puppeteer-core';
+
+const browser = await puppeteer.launch({
+    executablePath: 'A:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+});
+const page = await browser.newPage();
+
+await page.goto('https://www.yuque.com');
+
+await page.setViewport({width: 1920, height: 1080});
+
+await page.screenshot({path: 'test.png'});
+
+await browser.close();
